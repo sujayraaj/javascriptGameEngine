229933537fdbeeaaa3ad37ba4cc211c12d7d3db6
sujayraaj <sujayraaj@gmail.com>
2016-01-13 01:32:25 +0530

    Part of Mouse events added.
    Signed-off-by: sujayraaj <sujayraaj@gmail.com>

--- game.js
+++ game.js
@@ -1,3 +1,5 @@
+/* Keyboard Handling */
+
 function keyboard () {
     this.keyStateArr = [];
     this.wasPressedArr = [];
@@ -37,4 +39,42 @@ function keyUpHandler (evt) {
 window.onkeydown = function(evt) {keyDownHandler(evt)};
 window.onkeyup = function(evt) {keyUpHandler(evt)};
 keyMode();
+
+
+/* Handling the Mouse */
+var customCursor = false;
+var customCursorLink;
+var nameCanvas;
+var cursorVisible = true;
+var mousePos = {x:0,y:0};
+
+
+
+function showMouseCursor(canvasName){
+  if( customCursor ){
+      document.getElementById(canvasName).style.cursor = url(customCursorLink);
+  }
+  else {
+    document.getElementById('nocursor').style.cursor = auto;
+  }
+  cursorVisible = true;
+}
+
+function hideMouseCursor(canvasName){
+  document.getElementById(canvasName).style.cursor = none;
+  cursorVisible = false;
+}
+
+function getMousePosition(canvasName, evt) {
+  var rect = canvasName.getBoundingClientRect();
+  return {
+    x: evt.clientX - rect.left,
+    y: evt.clientY - rect.top
+  };
+}
+
+function mouseMovementTracker(evt){
+  mousePos = getMousePos(canvas, evt);
+}
+
 
